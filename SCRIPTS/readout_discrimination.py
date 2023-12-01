8304c2c54f95d1c76dfc4e831a982aa0746e9c6a
F-Michelon <michelonfrancois00@gmail.com>
2023-12-01 11:37:20 +0100

    ajout du script de discrimination de readout

---
+++ SCRIPTS/readout_discrimination.py
@@ -0,0 +1,18 @@
+# Ce script développe des methodes de discrimination de readout pour du multi classe
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+PATH = "../DONNEES/toy_datasets/readout_fictifs_D_3"
+
+D = pd.read_csv(PATH)
+
+# method 1
+""" Choisir séquentiellement les readouts les plus différents
+"""
+
+# method 2
+""" 
+"""
