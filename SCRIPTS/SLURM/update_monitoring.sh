21b06fee7612d06b4d71ae3cb88255229d8766bc
armelesper <armel.esper@eleves.ec-nantes.fr>
2024-03-07 15:35:03 +0100

    Changes to be committed: 	new file:   SCRIPTS/SLURM/update_monitoring.sh  Changes not staged for commit: 	deleted:    SCRIPTS/update_monitoring.sh

---
+++ SCRIPTS/SLURM/update_monitoring.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd ../../
+
+# Spécifiez le chemin du fichier à surveiller
+FILE="./RESULTATS/RESULTATS_NAUTILUS/K_CLASSES_WITH_ZEROS/TESTBASH.txt"
+
+# Commande d'exécution du programme
+clingo ./DONNEES/real_datasets/2023_PKN_earlyAndMediumAndLate_traite_2_asp_data.lp ./SCRIPTS/v10.2_no_parents_k_classes.lp > "$FILE" &
+
+# Boucle infinie pour surveiller les modifications du fichier
+while true; do
+    # Vérifier si le fichier a été modifié depuis la dernière sauvegarde
+    if [ -f "$FILE" ]; then
+        # Calculer le hachage MD5 du fichier
+        MD5SUM_OLD=$(md5sum "$FILE")
+	sleep 0.001
+        MD5SUM_NEW=$(md5sum "$FILE")
+
+        # Vérifier si le fichier a été modifié
+        if [ "$MD5SUM_OLD" != "$MD5SUM_NEW" ]; then
+            # Créer une copie de sauvegarde avec un timestamp
+            TIMESTAMP=$(date +"%Y%m%d_%H%M%S%N")
+            cp "$FILE" "${FILE}_backup_$TIMESTAMP"
+            echo "Sauvegarde créée : ${FILE}_backup_$TIMESTAMP"
+        fi
+    fi
+
+    # Attendre un certain temps avant de vérifier à nouveau
+    sleep 0.000001
+done
+
